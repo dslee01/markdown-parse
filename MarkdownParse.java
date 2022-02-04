51142db7fcf931b4a160e819ce3072c8c4731199
Dylan Lee <dylanlee650@gmail.com>
2022-02-03 16:40:06 -0800

    fixed failure inducing test

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -13,6 +13,9 @@ public class MarkdownParse {
         int currentIndex = 0;
         if (markdown.indexOf("(") != -1 && markdown.indexOf("[") != -1 && markdown.indexOf("[") == 0) {
             while (currentIndex < markdown.length()) {
+                if (markdown.indexOf("!") == 0) {
+                    break;
+                }
                 int nextOpenBracket = markdown.indexOf("[", currentIndex);
                 int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
                 int openParen = markdown.indexOf("(", nextCloseBracket);
